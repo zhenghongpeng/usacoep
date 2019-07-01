5a4c92629cb5ea670d228b4e3d7402b349ff5519
Zhenghong Peng <zhenghongpeng@Zhenghongs-MBP.hsd1.tx.comcast.net>
2019-06-30 23:17:54 -0500

    2017 cmp.sh

--- billboard2/billboard2.py
+++ billboard2/billboard2.py
@@ -3,8 +3,8 @@ ID: ericvpe4
 LANG: PYTHON3
 TASK: billboard
 """
-fin = open('billboard.in', 'r')
-fout = open('billboard.out', 'w')
+fin = open('billboard2.in', 'r')
+fout = open('billboard2.out', 'w')
 x1, y1, x2, y2 = map(int, fin.readline().split())
 x3, y3, x4, y4 = map(int, fin.readline().split())
 
--- crossroad/crossroad.py
+++ crossroad/crossroad.py
@@ -7,17 +7,19 @@ for x in range(N):
     pos.append(fin.readline().split())
     pos[x][0], pos[x][1] = int(pos[x][0]), int(pos[x][1])
     IDs.append(pos[x][0])
+    # print(pos)
 IDs = list(set(IDs))
+# print(IDs)
 count = 0
 for a in IDs:
     spec = []
     for b in pos:
         if b[0] == a:
             spec.append(b[1])
-    print(a,spec)
+    # print(a,spec)
     for c in range(len(spec)-1):
         if spec[c] != spec[c+1]:
             count += 1
-            print(a, b, c, spec, count)
+            # print(a, b, c, spec, count)
 fout.write(str(count)+'\n')
 
